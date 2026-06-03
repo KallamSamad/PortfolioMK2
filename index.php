9e4ef341dbab47a3c53372343b1ebb5c837c33bb
Kallam <kallamsamad@gmail.com>
2026-06-03 21:26:49 +0100

    Added styled Nav and good contrast between colors

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kallam Samad | Portfolio</title>
+    <title>Kallam Samad | Portfolio MK2</title>
     <link href="./src/output.css" rel="stylesheet">
 </head>
-<body class="bg-zinc-950 text-zinc-50 font-sans antialiased">
+<?php require_once "nav.php" ?> 
+<body class="bg-zinc-900 text-zinc-50 font-sans antialiased">
     
     <h1 class="text-3xl font-bold p-6 text-center">Portfolio 2.0 Under Construction 🚀</h1>
 </body>
---
+++ nav.php
@@ -0,0 +1,7 @@
+<div class="flex gap-4 justify-center pt-8 "><ul class=" rounded-xl list-none flex flex-row gap-4 bg-[#ff6f61] p-6">
+    <li><a href="#">About Me</a></li>
+    <li><a href="#">Projects</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">Resume</a></li>
+</ul>
+</div>
