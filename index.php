93bd05b11b01c8e0da81dec0acbd75e38087d01f
Kallam <kallamsamad@gmail.com>
2026-06-03 20:33:17 +0100

    Playing around with tailwind

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kallam Samad | Portfolio</title>
-    <!-- Point to the output file Tailwind will build -->
-    <link href="./dist/output.css" rel="stylesheet">
+    <link href="./src/output.css" rel="stylesheet">
 </head>
-<body class="bg-zinc-950 text-zinc-50 font-sans antialiased">
-    <!-- Your layout goes here -->
+<body class="bg-lime-500 text-zinc-50 font-sans antialiased">
+    
     <h1 class="text-3xl font-bold p-6 text-center">Portfolio 2.0 Under Construction 🚀</h1>
 </body>
 </html>
